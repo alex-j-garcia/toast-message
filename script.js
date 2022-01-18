a2677d1a0629714ec47b29f4377b8df126ffc865
Alex J Garcia <alexshuu@gmail.com>
2022-01-18 10:18:00 -0500

    Absolute positioning implemented

--- script.js
+++ script.js
@@ -2,15 +2,26 @@ document.getElementById("toast").addEventListener("change", displayToast);
 
 function displayToast(event) {
   const toastDialog = document.createElement("div");
-  toastDialog.innerText = event.target.checked ? "You checked the box!" :
+  toastDialog.textContent = event.target.checked ? "You checked the box!" :
     "You unchecked the box!";
   toastDialog.classList.add("toast");
+  toastDialog.style.top = setPositioning();
   document.body.appendChild(toastDialog);
   setTimer(toastDialog);
+}
+
+function setPositioning() {
+  let toasts = document.body.getElementsByClassName("toast");
+  let lastToast = toasts[toasts.length - 1];
+  if (lastToast) {
+    let bottom = lastToast.getBoundingClientRect().bottom;
+    return bottom + "px";
+  }
+  return 0;
 }
 
 function setTimer(node) {
   setTimeout(() => {
     document.body.removeChild(node);
-  }, 1300);
+  }, 3300);
 }
