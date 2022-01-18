895c947157fe4fa1535b414cf9e94d91b5fe65c9
Alex J Garcia <alexshuu@gmail.com>
2022-01-18 10:20:58 -0500

    Refactor - getPositioning

--- script.js
+++ script.js
@@ -5,19 +5,15 @@ function displayToast(event) {
   toastDialog.textContent = event.target.checked ? "You checked the box!" :
     "You unchecked the box!";
   toastDialog.classList.add("toast");
-  toastDialog.style.top = setPositioning();
+  toastDialog.style.top = getPositioning() + "px";
   document.body.appendChild(toastDialog);
   setTimer(toastDialog);
 }
 
-function setPositioning() {
+function getPositioning() {
   let toasts = document.body.getElementsByClassName("toast");
   let lastToast = toasts[toasts.length - 1];
-  if (lastToast) {
-    let bottom = lastToast.getBoundingClientRect().bottom;
-    return bottom + "px";
-  }
-  return 0;
+  return lastToast ? lastToast.getBoundingClientRect().bottom : 0;
 }
 
 function setTimer(node) {
