d264a954c05971615b0ad9df0ff7fd1a2020e75c
Alex J Garcia <alexshuu@gmail.com>
2022-01-21 20:15:54 -0500

    Toast messages recalculate their positions more efficiently.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.getElementById("toast").addEventListener("change", displayToast);
+document.getElementById("toast").addEventListener("change", createToast);
 
-function displayToast(event) {
+function createToast(event) {
   let {checked} = event.target;
   let toastDialog = document.createElement("div");
   toastDialog.innerHTML = `
@@ -12,7 +12,7 @@ function displayToast(event) {
   toastDialog.classList.add("toast");
   toastDialog.style.top = getInitialPosition();
   document.body.appendChild(toastDialog);
-  setTimers(toastDialog.lastElementChild);
+  setTimers(toastDialog);
 }
 
 function getInitialPosition() {
@@ -30,15 +30,33 @@ function getNewPosition(node) {
   }
 }
 
-function setTimers(progressBar) {
+// This function should use forEach to iterate over all the toasts
+// on the board and assign their new positions.
+
+// check/uncheck checkbox --> toast is added to the document.
+// it could be the only toast, in which case it won't return anything.
+// its position should be assigned to the next toast and every subsequent
+// toast should have its position assigned to that of the element before it.
+function calculatePositions() {
+  let toasts = Array.from(document.body.getElementsByClassName("toast"));
+  let top;
+  toasts.forEach((toast, index) => { // [0, 60, 120]
+    let currentTop = getComputedStyle(toast).top;
+    if (index != 0) toast.style.top = top;
+    top = currentTop;
+  });
+}
+
+function setTimers(node) {
+  let progressBar = node.lastElementChild;
   let currentWidth = getComputedStyle(progressBar).width;
   let intervalID = setInterval(() => {
     let widthNum = currentWidth.slice(0, -2);
     if (widthNum < 1.67) {
       clearInterval(intervalID);
-      document.body.removeChild(progressBar.offsetParent);
+      calculatePositions();
+      document.body.removeChild(node);
     } else {
-      getNewPosition(progressBar.offsetParent);
       currentWidth = progressBar.style.width = (widthNum - 1.67) + "px";
     }
   }, 25);
